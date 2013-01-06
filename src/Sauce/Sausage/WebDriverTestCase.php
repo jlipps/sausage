5611a607cb6f9850cd7b3ebef5951f6e9e4d93f2
Alexander Bayandin <a.bayandin@gmail.com>
2013-01-06 19:12:45 +0700

    Update src/Sauce/Sausage/WebDriverTestCase.php

--- src/Sauce/Sausage/WebDriverTestCase.php
+++ src/Sauce/Sausage/WebDriverTestCase.php
@@ -135,10 +135,10 @@ abstract class WebDriverTestCase extends \PHPUnit_Extensions_Selenium2TestCase
 
     public function prepareSession()
     {
-        parent::prepareSession();
+        $session = parent::prepareSession();
         if ($this->getBuild())
             SauceTestCommon::ReportBuild($this->getSessionId(), $this->getBuild());
-        return parent::prepareSession();
+        return $session;
     }
 
     public function tearDown()
